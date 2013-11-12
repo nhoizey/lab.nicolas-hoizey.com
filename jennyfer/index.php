310c18d476b7c9145b67b351dc4705f02707ccfa
Nicolas Hoizey <nicolas@hoizey.com>
2013-11-12 12:42:00 +0100

    load more

--- jennyfer/index.php
+++ jennyfer/index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>Jennyfer</title>
+	<script src="jquery-2.0.3.js"></script>
 	<script src="masonry.pkgd.min.js"></script>
 	<style>
 	body {
@@ -31,27 +32,52 @@
 		color: #fff;
 		opacity: .7;
 	}
+
+	.next {
+		text-align: center;
+	}
+
+	.next a, .next a:visited {
+		display: inline-block;
+		width: 50%;
+		padding: 5px;
+		font-weight: bold;
+		background-color: #666;
+		color: #fff;
+		text-decoration: none;
+	}
+
+	.next a:hover {
+		background-color: #669;
+		color: #ccf;
+	}
 	</style>
 </head>
 <body>
 <div id="container">
 	<?php
-	for ($i = 1; $i <= 30; $i++) {
-		if (rand(0,1) === 0) {
-			echo '<div class="item"><img src="img/rectangle-272x410-'.rand(1,5).'.png" width="272" height="410" /><p>'.$i.'</p></div>';
-		} else {
-			echo '<div class="item"><img src="img/carre-272x272-'.rand(1,5).'.png" width="272" height="272" /><p>'.$i.'</p></div>';
-		}
-	}
+	$start = isset($_GET['start']) ? $_GET['start'] : 1;
+	include_once 'products.php';
 	?>
 </div>
+<p class="next"><a href="?start=<?php echo $start + 1; ?>">Voir les produits suivants</a></p>
+
 <script>
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-  // options
-  // containerStyle: null,
-  // columnWidth: 282,
+$('#container').masonry({
   itemSelector: '.item'
+});
+
+$('.next a').on('click', function (event) {
+	event.preventDefault();
+	$this = $(this);
+	$.get('products.php' + $this.attr('href'), function(data) {
+		$data = $(data);
+		$container = $('#container');
+	  $container.append($data);
+	  $container.masonry('appended', $data);
+	  index = parseInt($this.attr('href').replace(/^.*=([0-9]+)$/, '$1'), 10);
+	  $this.attr('href', '?start=' + (index + 1));
+	});
 });
 </script>
 </body>
