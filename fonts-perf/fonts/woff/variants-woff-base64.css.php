72f3198fc4b23f90f134def2fc3cd3dfff48584e
Nicolas Hoizey <nicolas@hoizey.com>
2015-06-02 02:24:37 +0200

    All fonts need this

--- fonts-perf/fonts/woff/variants-woff-base64.css.php
+++ fonts-perf/fonts/woff/variants-woff-base64.css.php
@@ -1,6 +1,7 @@
 <?php
 sleep(1);
 header('Content-type: text/css');
+header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + 60*60*24*365));
 
 include 'SourceSansPro-LightIt.woff.css';
 include 'SourceSansPro-Semibold.woff.css';
